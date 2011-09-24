a05bcaad43bf7793b680caafbf6483863945ef4a
Leonel Santos <leonelsantos.me@gmail.com>
2011-09-24 16:15:48 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fundamento::Application.config.secret_token = '0cab3e9d939ec5e29b266f30eb2109d7016dbed5771204d4972a898ef76a1d25c7459f8f840c9d02791e6b5de630290b1304e0593b91ee6f76635e2f33fe7f4c'
